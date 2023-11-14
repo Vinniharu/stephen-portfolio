9b1e7de488ccd813555042c9893f4d11857e97af
Fagbelu Stephen <Fagbelustephen@gmail.com>
2023-11-14 22:16:54 +0100

    update to skill section

---
+++ script.js
@@ -0,0 +1,14 @@
+const links = document.getElementById("links");
+const navButton = document.getElementById("button");
+
+const navBar = () =>{
+   if(navButton.firstElementChild.innerHTML == "+"){
+      navButton.firstElementChild.innerHTML = "-";
+      links.style.animation = "dropdown 0.8s ease-in-out forwards";
+   } else{
+      navButton.firstElementChild.innerHTML = "+";
+      links.style.animation = "dropup 0.8s ease-in-out forwards";
+   }
+};
+
+navButton.addEventListener("click", navBar);
