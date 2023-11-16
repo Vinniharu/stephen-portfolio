7c352afdfb1b3969703d25a796c0db9897485f3f
Fagbelu Stephen <Fagbelustephen@gmail.com>
2023-11-16 14:31:28 +0100

    added animations || project section structure

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const links = document.getElementById("links");
 const navButton = document.getElementById("button");
+const holder_link = document.getElementById("links");
+const goToTop = document.getElementById("return");
+let holder_children = holder_link.children;
 
 const navBar = () =>{
-   if(navButton.firstElementChild.innerHTML == "+"){
-      navButton.firstElementChild.innerHTML = "-";
+   if(navButton.name == "menu-outline"){
+      navButton.name = "close-outline";
       links.style.animation = "dropdown 0.8s ease-in-out forwards";
    } else{
-      navButton.firstElementChild.innerHTML = "+";
+      navButton.name = "menu-outline";
       links.style.animation = "dropup 0.8s ease-in-out forwards";
    }
 
-   setTimeout(() =>{
-      navButton.firstElementChild.innerHTML = "+";
-      links.style.animation = "dropup 0.8s ease-in-out forwards";
+};
+
+const goToTopAnimation = () =>{
+   goToTop.style.animation = "dropdown 0.8s ease-in-out forwards";
+   setTimeout(() => {
+      goToTop.style.animation = "dropup 0.8s ease-in-out forwards";
    }, 5000);
 };
+
+const closeNav = () =>{
+   navButton.name = "menu-outline";
+   links.style.animation = "dropup 0.8s ease-in-out forwards";
+}
+
+document.body.addEventListener("scroll", goToTopAnimation);
 
 navButton.addEventListener("click", navBar);
